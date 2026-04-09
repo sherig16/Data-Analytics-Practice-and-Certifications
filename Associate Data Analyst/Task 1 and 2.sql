8dcfc6a8de34002b4e2451ba64cda1dd40169ecc
Sheri Grach <grachsheri@gmail.com>
2026-04-08 23:09:15 -0400

    Update Task 1 and 2.sql

--- Associate Data Analyst/Task 1 and 2.sql
+++ Associate Data Analyst/Task 1 and 2.sql
@@ -1,4 +1,4 @@
--- ## Task 1: 
+-- Task 1: 
 -- Write a query to determine how many products have the year_added value missing. Your output should be a single column, 
 -- missing_year, with a single row giving the number of missing values.
 
@@ -7,7 +7,7 @@ FROM products
 WHERE year_added IS NULL;
 
 
--- ## Task 2: 
+-- Task 2: 
 -- Write a query to ensure the product data matches the description provided. 
 -- Do not update the original table.
 
